dc5ce2c7aed666aad7eef558cc57c5bd353274df
Michael A <maalbaneze@aol.com>
2019-04-10 08:54:00 -0700

    WIP

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Three Cheeses');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Swiss and Onion');
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Bacon Bleu', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Marilyn Manson', true);
