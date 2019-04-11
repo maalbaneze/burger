7fb965905c23616b8b6cfdb4481bc669b641bdcb
Michael A <maalbaneze@aol.com>
2019-04-11 06:51:18 -0700

    Almost

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
+// Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
@@ -18,9 +18,9 @@ router.get("/", function (req, res) {
 
 router.post("/api/burgers", function (req, res) {
     burger.create([
-        "name", "eaten"
+        "name", "devoured"
     ], [
-            req.body.name, req.body.eaten
+            req.body.name, req.body.devoured
         ], function (result) {
             // Send back the ID of the new burger
             res.json({ id: result.insertId });
@@ -33,7 +33,7 @@ router.put("/api/burgers/:id", function (req, res) {
     console.log("condition", condition);
 
     burger.update({
-        eaten: req.body.eaten
+        devoured: req.body.devoured
     }, condition, function (result) {
         if (result.changedRows == 0) {
             // If no rows were changed, then the ID must not exist, so 404
