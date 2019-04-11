d547b2944a5f2ea2e4ff1589c5b925a5deff9eff
Michael A <maalbaneze@aol.com>
2019-04-11 07:15:50 -0700

    update

--- config/orm.js
+++ config/orm.js
@@ -41,7 +41,7 @@ function objToSql(ob) {
 
 // Object for all our SQL statement functions.
 var orm = {
-    all: function (tableInput, cb) {
+    selectAll: function (tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
         connection.query(queryString, [tableInput], function (err, result) {
             if (err) {
@@ -50,7 +50,7 @@ var orm = {
             cb(result);
         });
     },
-    create: function (table, cols, vals, cb) {
+    insertOne: function (table, cols, vals, cb) {
         var queryString = "INSERT INTO " + table;
 
         queryString += " (";
@@ -70,8 +70,8 @@ var orm = {
             cb(result);
         });
     },
-    // An example of objColVals would be {name: panther, sleepy: true}
-    update: function (table, objColVals, condition, cb) {
+    // An example of objColVals would be {name: cheesburger, devoured: true}
+    updateOne: function (table, objColVals, condition, cb) {
         var queryString = "UPDATE " + table;
 
         queryString += " SET ";
--- models/burger.js
+++ models/burger.js
@@ -2,20 +2,20 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-    all: function (cb) {
+    selectAll: function (cb) {
         //res is the result from the database
-        orm.all("burgers", function (res) {
+        orm.selectAll("burgers", function (res) {
             cb(res);
         });
     },
     // The variables cols and vals are arrays.
-    create: function (cols, vals, cb) {
-        orm.create("burgers", cols, vals, function (res) {
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function (res) {
             cb(res);
         });
     },
-    update: function (objColVals, condition, cb) {
-        orm.update("burgers", objColVals, condition, function (res) {
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
             cb(res);
         });
     },
